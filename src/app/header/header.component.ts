ff01f37fa7a4ab0c8a34ecc5cb40cd57a2067d49
Duc Truong <phuduc1193@gmail.com>
2017-03-15 00:24:11 -0500

    Responsive Layouts and Functionalities

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { GlobalService } from '../services/global.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  host: {
+    '(document:click)': 'documentClick($event)'
+  }
 })
 export class HeaderComponent implements OnInit {
   theTitle: String;
+  activeNav: boolean;
 
-  constructor(private _globals:GlobalService) {
+  constructor(private _globals:GlobalService, private _eref: ElementRef) {
     let splitTitle: String[];
     splitTitle = _globals.pageTitle.split(' ');
     this.theTitle = splitTitle[0] + '<span>' + splitTitle[1] + '</span>';
+    this.activeNav = false;
   }
 
   ngOnInit() {
   }
 
+  isActive() {
+    if (this.activeNav)
+      return " active";
+    else return "";
+  }
+
+  documentClick(event) {
+    if (event.target != document.querySelector('i.fa.fa-bars'))
+      this.activeNav = false;
+  }
 }
